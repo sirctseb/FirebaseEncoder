2ecc7ce5e31b419ec7b51158a13a78d5c02ca275
Christopher Best <bestchris@gmail.com>
2016-07-15 10:50:53 -0400

    implement add test remove deps remove html file

--- lib/firebase_encode.dart
+++ lib/firebase_encode.dart
@@ -1,5 +1,19 @@
 library firebase_encode;
 
-String encode(String key) {}
+String encode(String key) {
+  return key.replaceAllMapped(new RegExp(r'[\.\$\#\[\]/%]'),
+      (match) => '%${match.group(0).codeUnitAt(0)}');
+}
 
-String decode(String key) {}
+String decode(String key) {
+  var codeToChar = {
+    '%46': r'.',
+    '%36': r'$',
+    '%91': r'[',
+    '%93': r']',
+    '%47': r'/',
+    '%37': r'%'
+  };
+  return key.replaceAllMapped(new RegExp(r'%46|%36|%91|%93|%47|%37'),
+      (match) => codeToChar[match.group(0)]);
+}
--- test/firebase_encode_test.dart
+++ test/firebase_encode_test.dart
@@ -1,12 +1,49 @@
-import 'package:firebase_encode/firebase_encode.dart';
-import 'package:logging/logging.dart';
-import 'package:logging_handlers/logging_handlers_shared.dart';
+import 'package:firebase_encode/firebase_encode.dart' as fbe;
 import 'package:test/test.dart';
 
 main() {
-  hierarchicalLoggingEnabled = true;
-  Logger.root.onRecord.listen(new LogPrintHandler());
-  Logger.root.level = Level.INFO;
+  group('FirebaseEncode', () {
+    test('encodes .', () {
+      expect(fbe.encode(r'a.a'), r'a%46a');
+    });
+    test(r'encodes $', () {
+      expect(fbe.encode(r'a$a'), r'a%36a');
+    });
+    test(r'encodes [', () {
+      expect(fbe.encode(r'a[a'), r'a%91a');
+    });
+    test(r'encodes ]', () {
+      expect(fbe.encode(r'a]a'), r'a%93a');
+    });
+    test(r'encodes /', () {
+      expect(fbe.encode(r'a/a'), r'a%47a');
+    });
+    test(r'encodes %', () {
+      expect(fbe.encode(r'a%a'), r'a%37a');
+    });
 
-  group('FirebaseEncode', () {});
+    test(r'decodes .', () {
+      expect(fbe.decode(r'a%46a'), r'a.a');
+    });
+    test(r'decodes $', () {
+      expect(fbe.decode(r'a%36a'), r'a$a');
+    });
+    test(r'decodes [', () {
+      expect(fbe.decode(r'a%91a'), r'a[a');
+    });
+    test(r'decodes ]', () {
+      expect(fbe.decode(r'a%93a'), r'a]a');
+    });
+    test(r'decodes /', () {
+      expect(fbe.decode(r'a%47a'), r'a/a');
+    });
+    test(r'decodes %', () {
+      expect(fbe.decode(r'a%37a'), r'a%a');
+    });
+
+    test('decode inverses encode', () {
+      var string = r'.$[]/%';
+      expect(string, fbe.decode(fbe.encode(string)));
+    });
+  });
 }
